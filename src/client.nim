f4e20607f42d47202c0a23abfcbf791829438494
Juan Carlos <juancarlospaco@gmail.com>
2024-05-19 13:53:58 -0300

    init

--- src/client.nim
+++ src/client.nim
@@ -1,12 +1,12 @@
 # Websockets-based client for Elixir Phoenix Channels,
 # that also implements a Supabase Realtime client on top.
 # Elixir Phoenix Channels are basically just a websocket.
-import whisky
+import whisky  # https://github.com/guzba/whisky
 
 type
   RealtimeClient* = object
+    url*, channel*: string
     client: WebSocket
-    url: string
 
   SREvents* {.pure.} = enum
     close     = "phx_close"
@@ -18,15 +18,21 @@ type
 
 proc close*(self: RealtimeClient) {.inline.} = self.client.close()
 
-func connect*(self: RealtimeClient) {.inline.} = discard  # Do nothing.
+func connect*(self: RealtimeClient) {.inline.} = discard  # Do nothing?.
 
 proc newRealtimeClient*(url: string): RealtimeClient = RealtimeClient(url: url, client: newWebSocket(url))
 
-proc listen*(self: RealtimeClient): auto =
-  discard
-
-proc setChannel*(self: RealtimeClient; topic: string): auto =
-  discard
+template setChannel*(self: RealtimeClient; topic: string) =
+  # https://github.com/supabase-community/realtime-py/blob/8bcf6da63e161d7127292a079887952d2c8a2722/realtime/connection.py#L142-L148
+  if topic.len > 0: self.channel = topic
 
 proc summary*(self: RealtimeClient) =
+  # https://github.com/supabase-community/realtime-py/blob/8bcf6da63e161d7127292a079887952d2c8a2722/realtime/connection.py#L152-L159
+  #for topic, chans in self.channels.items():
+  #  for chan in chans:
+  #    echo "Topic: ", topic, " | Events: ", chan
+  discard
+
+proc listen*(self: RealtimeClient): auto =
+  # https://github.com/supabase-community/realtime-py/blob/8bcf6da63e161d7127292a079887952d2c8a2722/realtime/connection.py#L59-L66
   discard
